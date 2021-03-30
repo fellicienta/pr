e48c7b52e10a6cc06b7e6692adae6828ab3e1f21
Dariia Romantseva <fellicienta@gamil.com>
2021-03-30 16:39:05 +0300

    initial commit

--- adder/adder.cpp
+++ adder/adder.cpp
@@ -4,14 +4,12 @@
 #include <random>
 
 int32_t logical_add(int32_t number1, int32_t number2) {
-    uint32_t x;
-    uint32_t y;
     int32_t result = 0;
     uint32_t q = 0;
 
     for (uint32_t i = 0; i < 32; ++i) {
-        x = (number1 >> i) & 0x01;
-        y = (number2 >> i) & 0x01;
+        const uint32_t x = (number1 >> i) & 0x01;
+        const uint32_t y = (number2 >> i) & 0x01;
         result += ((x ^ y ^ q) << i);
         q = x & y ^ (x ^ y) & q;      
     }
