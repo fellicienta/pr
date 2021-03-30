009cdc7c3b6af9b359f1bb2a2e45147282b955e5
Dariia Romantseva <fellicienta@gamil.com>
2021-03-30 11:08:13 +0300

    initial commit

--- adder/adder.cpp
+++ adder/adder.cpp
@@ -2,9 +2,28 @@
 
 #include <stdint.h>
 
-template <typename T>
-T logical_add(T a, T b) {
-    uint32_t size = sizeof(T) * 8;  //quantity of bits
+int logical_add(int number1, int number2) {
+    uint32_t size = sizeof(int) * 8;  //quantity of bits
+    int mask = 1;
+    int q = 0;
+    int x;
+    int y;
+    int z;
+    int result = 0;
 
-    return size;
+    for (int i = 0; i < size; ++i) {
+        x = x & mask;
+        y = y & mask;
+        z = x ^ y ^ q;
+        result += (z << i);
+
+        q = x & y ^ (x ^ y) & q; 
+        number1 >>= 1;
+        number2 >>= 1;
+    }
+
+    z = q;
+ //   result += (z << 31);
+
+    return result;
 }
--- adder/main.cpp
+++ adder/main.cpp
@@ -1,12 +1,13 @@
 #include "adder.h"
 
 #include <iostream>
+#include <assert.h>
 
 int main() {
     int a = 5;
     int b = 3;
-
+ //   assert((5+3) == logical_add(a, b));
     std::cout << logical_add(a, b) << std::endl;
-    
+
     return 0;
 }
