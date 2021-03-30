26bd1a3671890772b9a6de7dfd077cf544fff699
Dariia Romantseva <fellicienta@gamil.com>
2021-03-30 12:16:59 +0300

    initial commir

--- adder/Test.cpp
+++ adder/Test.cpp
@@ -3,9 +3,30 @@
 #include <gtest/gtest.h>
 
 TEST(adder, positiv_numbers) {
-    EXPECT_EQ((5 + 5), logical_add(5, 5));
+    uint32_t a = rand_number();
+    uint32_t b = rand_number();
+    
+    EXPECT_EQ((a + b), logical_add(a, b));
 }
 
-TEST(adder, negativ_numbers) {
-    EXPECT_EQ((-5 + 5), logical_add(-5, 5));
+TEST(adder, 1negativ_number) {
+    uint32_t a = rand_number();
+    uint32_t b = rand_number();
+    
+    EXPECT_EQ((-a + b), logical_add(-a, b));
+}
+
+TEST(adder, 2negativ_numbers) {
+    uint32_t a = rand_number();
+    uint32_t b = rand_number();
+    
+    EXPECT_EQ((-a + -b), logical_add(-a, -b));
+}
+
+TEST(adder, big_numbers) {
+    EXPECT_EQ((999999 + 999999), logical_add(999999, 999999));
+}
+
+TEST(adder, zero) {
+    EXPECT_EQ((0 + 0), logical_add(0, 0));
 }
--- adder/adder.h
+++ adder/adder.h
@@ -2,3 +2,4 @@
 #include <stdint.h>
 
 uint32_t logical_add(uint32_t number1, uint32_t number2);
+uint32_t rand_number();
