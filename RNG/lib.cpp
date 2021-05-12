5dc73ad64472c360ebb69d168f3c463b09fce4a8
Dariia Romantseva <fellicienta@gmail.com>
2021-05-12 12:32:55 +0300

    update

--- RNG/lib.cpp
+++ RNG/lib.cpp
@@ -9,39 +9,35 @@ std::mt19937 gen(rd());
 Statistic uniform_distribution(const int32_t a, const int32_t b, const uint32_t sample_size)
 {
     std::uniform_int_distribution<> distribution(a, b);
-    Statistic statistic{};
+    Statistic st;
 
     for (uint32_t i = 0; i < sample_size; ++i)
-        ++statistic[distribution(gen)];
+        ++st[distribution(gen)];
 
-    return statistic;
+    return st;
 }
 
 Statistic normal_distribution(const int32_t mean, const uint32_t deviation,
                               const uint32_t sample_size)
 {
     std::normal_distribution<> distribution{mean, deviation};
-
-    Statistic statistic{};
+    Statistic st;
 
     for (uint32_t n = 0; n < sample_size; ++n)
-    {
-        ++statistic[std::round(distribution(gen))];
-    }
-    return statistic;
+        ++st[std::round(distribution(gen))];
+
+    return st;
 }
 
 Statistic exponential_distribution(const uint32_t time_unit, const uint32_t sample_size)
 {
     std::exponential_distribution<> distribution(time_unit);
-
-    Statistic statistic{};
+    Statistic st;
 
     for (uint32_t n = 0; n < sample_size; ++n)
-    {
-        ++statistic[distribution(gen)];
-    }
-    return statistic;
+        ++st[distribution(gen)];
+
+    return st;
 }
 
 void save_statistic(const std::string &file_name, const Statistic &st)
@@ -54,9 +50,7 @@ void save_statistic(const std::string &file_name, const Statistic &st)
          << "FREQUENCY" << std::endl;
 
     for (auto i : st)
-    {
         file << i.first << ',' << i.second << std::endl;
-    }
 
     file.close();
 }
--- RNG/main.cpp
+++ RNG/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    save_statistic("uniform.csv", uniform_distribution(1, 500, 10000));
+    save_statistic("uniform.csv", uniform_distribution(1, 100, 10000));
     save_statistic("normal.csv", normal_distribution(50, 10, 10000));
     save_statistic("exponential.csv", exponential_distribution(1, 10000));
 
