2c5f1d9f7de10ec75bf3a1318853215b0924fb6a
Dariia Romantseva <fellicienta@gmail.com>
2021-05-14 17:15:18 +0300

    initial commit

--- check-brackets/lib.cpp
+++ check-brackets/lib.cpp
@@ -5,6 +5,10 @@
 std::string check_brackets(std::string str)
 {
     bool is_punct(char c);
+    bool is_open_bracket(char c);
+    bool is_text(char c);
+    bool is_couple(char top, char c);
+
     uint32_t counter = 0;
     std::stack<char> st;
     std::stack<char> error_pos;
@@ -12,20 +16,19 @@ std::string check_brackets(std::string str)
     for (auto c : str)
     {
         ++counter;
-        if (c == '(' || c == '[' || c == '{')
+        if (is_open_bracket(c))
         {
             st.push(c);
             error_pos.push(counter);
         }
-        else if (isalpha(c) || isdigit(c) || is_punct(c))
+        else if (is_text(c))
             continue;
         else
         {
             if (st.empty())
                 return std::to_string(counter);
 
-            char top = st.top();
-            if ((top == '(' && c == ')') || (top == '[' && c == ']') || (top == '{' && c == '}'))
+            if (is_couple(st.top(), c))
             {
                 st.pop();
                 error_pos.pop();
@@ -43,4 +46,19 @@ std::string check_brackets(std::string str)
 bool is_punct(char c)
 {
     return ispunct(c) && c != ']' && c != '}' && c != ')';
+}
+
+bool is_open_bracket(char c)
+{
+    return c == '(' || c == '[' || c == '{';
+}
+
+bool is_text(char c)
+{
+    return isalpha(c) || isdigit(c) || is_punct(c);
+}
+
+bool is_couple(char top, char c)
+{
+    return (top == '(' && c == ')') || (top == '[' && c == ']') || (top == '{' && c == '}');
 }
