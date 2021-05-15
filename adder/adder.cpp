9e722d0f8b0f86902d3c6da6a625aae2c5a340a2
Dariia Romantseva <fellicienta@gmail.com>
2021-05-15 15:15:10 +0300

    update

--- adder/adder.cpp
+++ adder/adder.cpp
@@ -1,23 +1,26 @@
 #include "adder.h"
 
-#include <stdint.h>
 #include <random>
+#include <stdint.h>
 
-int32_t logical_add(int32_t number1, int32_t number2) {
+int32_t logical_add(const int32_t number1, const int32_t number2)
+{
     int32_t result = 0;
     uint32_t q = 0;
 
-    for (uint32_t i = 0; i < 32; ++i) {
+    for (uint32_t i = 0; i < 32; ++i)
+    {
         const uint32_t x = (number1 >> i) & 0x01;
         const uint32_t y = (number2 >> i) & 0x01;
         result += ((x ^ y ^ q) << i);
-        q = x & y ^ (x ^ y) & q;      
+        q = x & y ^ (x ^ y) & q;
     }
 
     return result;
 }
 
-int32_t rand_number() {
+int32_t rand_number()
+{
     static std::random_device rd;
     static std::mt19937 g(rd());
 
--- adder/adder.h
+++ adder/adder.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <stdint.h>
 
-int32_t logical_add(int32_t number1, int32_t number2);
+int32_t logical_add(const int32_t number1, const int32_t number2);
 int32_t rand_number();
