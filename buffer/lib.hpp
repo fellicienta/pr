283f4195ad5b27f7fe5e0905ce47c3dc43d11eee
Dariia Romantseva <fellicienta@gmail.com>
2021-05-15 15:21:20 +0300

    update

--- buffer/lib.hpp
+++ buffer/lib.hpp
@@ -1,50 +1,57 @@
 #pragma once
-#include <vector>
-#include <stdint.h>
 #include <iterator>
+#include <stdint.h>
+#include <vector>
 
-template <typename T>
-class Rolling_buffer 
+template<typename T>
+class Rolling_buffer
 {
 public:
-    Rolling_buffer(const uint32_t size = 10) {
+    Rolling_buffer(const uint32_t size = 10)
+    {
         queue.resize(size);
         queue.clear();
     }
-    uint32_t size() {
-         return queue.size();
+    uint32_t size()
+    {
+        return queue.size();
     }
-    uint32_t capacity() {
+    uint32_t capacity()
+    {
         return queue.capacity();
     }
-    void push(T item) {
-        if (queue.size() < queue.capacity()) {
-            queue.push_back(item);
-        }
-        else {
+    void push(T item)
+    {
+        if (queue.size() >= queue.capacity())
             queue.erase(queue.begin(), queue.begin() + queue.capacity() / 2);
-            queue.push_back(item);
-        }
+
+        queue.push_back(item);
     }
-    typename std::vector<T>::iterator begin() {
+    typename std::vector<T>::iterator begin()
+    {
         return queue.begin();
     }
-    typename std::vector<T>::iterator end() {
+    typename std::vector<T>::iterator end()
+    {
         return queue.end();
     }
 
-    T & operator[](uint32_t index) {
+    T &operator[](uint32_t index)
+    {
         return queue.at(index);
     }
-    bool empty() {
+    bool empty()
+    {
         return queue.empty();
-    } 
+    }
+
 private:
     std::vector<T> queue;
 
-    T pop() {
+    T pop()
+    {
         if (queue.empty())
-        return -1;
+            return -1;
 
         T item = queue.front();
         queue.erase(queue.begin());
