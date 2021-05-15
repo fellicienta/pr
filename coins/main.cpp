5e111f11a62e6cc65047149d003f7b1a3e770117
Dariia Romantseva <fellicienta@gmail.com>
2021-05-15 15:33:16 +0300

    update

--- coins/main.cpp
+++ coins/main.cpp
@@ -1,19 +1,20 @@
+#include "coins.h"
 #include <iostream>
 #include <random>
-#include "coins.h"
 
-int main() {
-	const uint16_t REQUIRED = 11;
-	const uint16_t quantity = 1000;
-	uint32_t average = 0;
+int main()
+{
+    const uint16_t required = 11;
+    const uint16_t quantity = 1000;
+    uint32_t average = 0;
 
-	for (uint16_t i = 0; i < quantity; ++i) {
-		average += play(REQUIRED);
-	}
+    for (uint16_t i = 0; i < quantity; ++i)
+    {
+        average += play(required);
+    }
 
-	average /= quantity;
-	std::cout << "Average : " << average << std::endl;
+    average /= quantity;
+    std::cout << "Average : " << average << std::endl;
 
     return 0;
 }
-
