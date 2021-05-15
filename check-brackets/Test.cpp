756da723f6ca470f57f806b0f254bbd65b8c9ddd
fellicienta <72398324+fellicienta@users.noreply.github.com>
2021-05-15 16:35:38 +0300

    Update Test.cpp

--- check-brackets/Test.cpp
+++ check-brackets/Test.cpp
@@ -5,35 +5,35 @@
 TEST(CorrectBrackets, one_couple)
 {
     std::string s("[]");
-    EXPECT_EQ("Success", check_brackets(find_error_pos(s)));
+    EXPECT_EQ("Success", check_result(check_brackets(s)));
 }
 
 TEST(CorrectBrackets, many_couples)
 {
     std::string s("([](){([])})");
-    EXPECT_EQ("Success", check_brackets(find_error_pos(s)));
+    EXPECT_EQ("Success", check_result(check_brackets(s)));
 }
 
 TEST(CorrectBrackets, function_call)
 {
     std::string s("foo(bar);");
-    EXPECT_EQ("Success", check_brackets(find_error_pos(s)));
+    EXPECT_EQ("Success", check_result(check_brackets(s)));
 }
 
 TEST(IncorrectBrackets, 1_case)
 {
     std::string s("[]([]");
-    EXPECT_EQ("3", check_brackets(find_error_pos(s)));
+    EXPECT_EQ("3", check_result(check_brackets(s)));
 }
 
 TEST(IncorrectBrackets, 2_case)
 {
     std::string s("{[}");
-    EXPECT_EQ("3", check_brackets(find_error_pos(s)));
+    EXPECT_EQ("3", check_result(check_brackets(s)));
 }
 
 TEST(IncorrectBrackets, function_call)
 {
     std::string s("foo(bar[i);");
-    EXPECT_EQ("10", check_brackets(find_error_pos(s)));
+    EXPECT_EQ("10", check_result(check_brackets(s)));
 }
