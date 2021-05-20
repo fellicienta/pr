0e0cba6c54b85d868bd3ac815584569272e4e7bd
Dariia Romantseva <fellicienta@gmail.com>
2021-05-20 16:45:09 +0300

    update

--- boost-test/main.cpp
+++ boost-test/main.cpp
@@ -1,14 +1,33 @@
 #include <boost/program_options.hpp>
 #include <iostream>
+
 namespace po = boost::program_options;
+
+const std::map<std::string, std::function<int32_t(const int32_t, const int32_t)>> CALCULATOR = {
+    {"add", [](int32_t a, int32_t b) { return a + b; }},
+    {"sub", [](int32_t a, int32_t b) { return a - b; }},
+    {"mul", [](int32_t a, int32_t b) { return a * b; }},
+    {"div", [](int32_t a, int32_t b) { return a / b; }}};
+
+const std::map<std::string, std::string> OPERATIONS = {{"add", "Add values"},
+                                                       {"sub", "Subtract values"},
+                                                       {"mul", "Multiply values"},
+                                                       {"div", "Divide values"}};
+
+void print_operation(const std::string operation, const int32_t arg1, const int32_t arg2)
+{
+    std::cout << operation << ": " << CALCULATOR.at(operation)(arg1, arg2) << std::endl;
+}
 
 int main(int ac, char *av[])
 {
     try
     {
         po::options_description generic("Generic options");
-        generic.add_options()("help", "produce help message")("add", "Add values")(
-            "sub", "Subtract values")("mul", "Multiply values")("div", "Divide values");
+        generic.add_options()("help", "Produce help message");
+
+        for (auto x : OPERATIONS)
+            generic.add_options()(x.first.c_str(), x.second.c_str());
 
         po::options_description hidden("Hidden options");
         hidden.add_options()("values", po::value<std::vector<int32_t>>(), "values");
@@ -25,44 +44,35 @@ int main(int ac, char *av[])
 
         if (vm.count("help"))
         {
-            std::cout << generic << "\n";
+            std::cout << generic << std::endl;
             return 0;
         }
-
-        if (vm.count("values"))
-        {
-            std::vector<int32_t> args(vm["values"].as<std::vector<int32_t>>());
-            bool operation = false;
-            if (vm.count("add"))
-            {
-                std::cout << "Add: " << args[0] + args[1] << std::endl;
-                operation = true;
-            }
-            if (vm.count("sub"))
-            {
-                std::cout << "Sub: " << args[0] - args[1] << std::endl;
-                operation = true;
-            }
-            if (vm.count("mul"))
-            {
-                std::cout << "Mul: " << args[0] * args[1] << std::endl;
-                operation = true;
-            }
-            if (vm.count("div"))
-            {
-                std::cout << "Div: " << args[0] / args[1] << std::endl;
-                operation = true;
-            }
-
-            if (!operation)
-            {
-                std::cout << "Error: no operation is entered." << std::endl;
-            }
-        }
-        else
+        if (!vm.count("values"))
         {
             std::cout << "Error: the required arguments are missing." << std::endl;
+            return 1;
         }
+
+        std::vector<int32_t> args(vm["values"].as<std::vector<int32_t>>());
+        if (args.size() != 2)
+        {
+            std::cout << "Error: invalid number of arguments." << std::endl;
+            return 1;
+        }
+
+        bool option = false; // is any operation entered ?
+
+        for (auto x : OPERATIONS)
+        {
+            if (vm.count(x.first))
+            {
+                print_operation(x.first, args[0], args[1]);
+                option = true;
+            }
+        }
+
+        if (!option)
+            std::cout << "Error: no operation is entered." << std::endl;
     }
     catch (std::exception &e)
     {
