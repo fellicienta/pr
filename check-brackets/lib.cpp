ac3214456cfc2a40bfb7b82f616d74a85a090910
Dariia Romantseva <fellicienta@gmail.com>
2021-05-20 14:53:48 +0300

    update

--- check-brackets/lib.cpp
+++ check-brackets/lib.cpp
@@ -5,25 +5,22 @@
 #include <stack>
 
 const uint32_t SUCCESS = std::numeric_limits<uint32_t>::max();
-std::map<char, char> BRACKETS;
+std::map<char, char> BRACKETS = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
 
 namespace
 {
 bool is_punct(const char c)
 {
-    if (ispunct(c))
-        for (auto x : BRACKETS)
-            if (c == x.first || c == x.second)
-                return false;
-    return true;
+    return ispunct(c) &&
+           !std::any_of(BRACKETS.begin(), BRACKETS.end(), [&](const std::pair<char, char> &x) {
+               return c == x.first || c == x.second;
+           });
 }
 
 bool is_open_bracket(const char c)
 {
-    for (auto x : BRACKETS)
-        if (c == x.first)
-            return true;
-    return false;
+    return std::any_of(BRACKETS.begin(), BRACKETS.end(),
+                       [&](const std::pair<char, char> &x) { return c == x.first; });
 }
 
 bool is_text(const char c)
@@ -31,19 +28,15 @@ bool is_text(const char c)
     return isalpha(c) || isdigit(c) || is_punct(c);
 }
 
-bool is_couple(char top, const char c)
+bool is_couple(const char top, const char c)
 {
-    for (auto x : BRACKETS)
-        if (top == x.first && c == x.second)
-            return true;
-    return false;
+    return BRACKETS[top] == c;
 }
 
 } // namespace
 
 uint32_t check_brackets(const std::string &str)
 {
-    set_brackets();
     uint32_t counter = 0;
     std::stack<std::pair<char, uint32_t>> st;
 
@@ -51,48 +44,21 @@ uint32_t check_brackets(const std::string &str)
     {
         ++counter;
         if (is_text(c))
-        {
             continue;
-        }
         else if (is_open_bracket(c))
-        {
             st.emplace(c, counter);
-        }
         else
         {
-            if (st.empty())
-            {
+            if (st.empty() || !is_couple(st.top().first, c))
                 return counter;
-            }
-            if (is_couple(st.top().first, c))
-            {
-                st.pop();
-            }
-            else
-            {
-                return counter;
-            }
+            st.pop();
         }
     }
 
-    if (st.empty())
-    {
-        return SUCCESS;
-    }
-    else
-    {
-        return st.top().second;
-    }
+    return st.empty() ? SUCCESS : st.top().second;
 }
 
 std::string check_result(const int32_t result)
 {
     return result == SUCCESS ? "Success" : std::to_string(result);
-}
-
-void set_brackets()
-{
-    BRACKETS.emplace('(', ')');
-    BRACKETS.emplace('{', '}');
-    BRACKETS.emplace('[', ']');
 }
