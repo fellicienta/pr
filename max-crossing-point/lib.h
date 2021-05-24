91057f22761f206538d3ef1b7d45457b9a0d10e1
fellicienta <72398324+fellicienta@users.noreply.github.com>
2021-05-24 16:59:45 +0300

    Update lib.h

--- max-crossing-point/lib.h
+++ max-crossing-point/lib.h
@@ -5,6 +5,6 @@
 
 using Point = int32_t;
 using OverlapPointSet = std::set<Point>;
-using LineSegmentSet = std::map<Point, Point>;
+using LineSegmentSet = std::multimap<Point, Point>;
 
 OverlapPointSet find_max_overlap_point(LineSegmentSet set);
