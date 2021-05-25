5996d40057cd4a52e253680b1b8c14ff150c1803
Dariia Romantseva <fellicienta@gmail.com>
2021-05-25 14:20:34 +0300

    update

--- max-crossing-point/Test.cpp
+++ max-crossing-point/Test.cpp
@@ -23,6 +23,41 @@ TEST(main, case_3)
     EXPECT_EQ(result, find_max_overlap_point(set));
 }
 
+TEST(main, case_4)
+{
+    LineSegmentSet set = {{1, 4}, {5, 7}, {2, 3}, {-1, 0}};
+    OverlapPointSet result = {2, 3};
+    EXPECT_EQ(result, find_max_overlap_point(set));
+}
+
+TEST(main, case_5)
+{
+    LineSegmentSet set = {{1, 9}, {6, 14}, {3, 9}, {5, 13}, {2, 7}};
+    OverlapPointSet result = {6, 7};
+    EXPECT_EQ(result, find_max_overlap_point(set));
+}
+
+TEST(main, case_6)
+{
+    LineSegmentSet set = {{0, 3}, {2, 6}, {5, 7}};
+    OverlapPointSet result = {2, 3, 5, 6};
+    EXPECT_EQ(result, find_max_overlap_point(set));
+}
+
+TEST(main, no_overlap)
+{
+    LineSegmentSet set = {{0, 3}, {5, 7}};
+    OverlapPointSet result = {};
+    EXPECT_EQ(result, find_max_overlap_point(set));
+}
+
+TEST(main, single_segment)
+{
+    LineSegmentSet set = {{0, 3}};
+    OverlapPointSet result = {};
+    EXPECT_EQ(result, find_max_overlap_point(set));
+}
+
 TEST(main, empty_set)
 {
     LineSegmentSet set;
--- max-crossing-point/lib.cpp
+++ max-crossing-point/lib.cpp
@@ -1,40 +1,75 @@
 #include "lib.h"
 
 #include <algorithm>
+#include <iostream>
+#include <iterator>
 
 using Frequencies = std::map<Point, uint32_t>;
 using Frequency = Frequencies::const_iterator;
 
 namespace
 {
-Frequencies get_overlap_frequency(const LineSegmentSet &set)
+Frequencies get_overlap_frequency(const LineSegmentSet &set, const Point begin, const Point end)
 {
     Frequencies f;
-    for (auto segment : set)
-    {
-        for (int32_t i = segment.first; i <= segment.second; ++i)
-            ++f[i];
-    }
 
+    for (Point i = begin; i <= end; ++i)
+    {
+        for (auto segment : set)
+        {
+            if (i >= segment.first && i <= segment.second)
+                ++f[i];
+        }
+    }
     return f;
 }
-uint32_t get_max_frequency(const Frequencies &f)
+Frequency find_max_frequency(const Frequencies &f)
 {
-    Frequency it = std::max_element(
+    return std::max_element(
         f.begin(), f.end(),
         [](const std::pair<uint32_t, uint32_t> &p1, const std::pair<uint32_t, uint32_t> &p2) {
             return p1.second < p2.second;
         });
+}
+std::pair<Point, Point> find_search_boundaries(const LineSegmentSet &set)
+{
+    Frequencies f_end;
 
-    return it != f.end() ? it->second : 0;
+    for (auto segment : set)
+        ++f_end[segment.second];
+
+    Frequency it_end = find_max_frequency(f_end);
+    Point end = it_end->second == 1 ? f_end.rbegin()->first : it_end->first;
+
+    Frequencies f_begin;
+
+    for (auto segment : set)
+    {
+        if (segment.first > end)
+            break;
+        ++f_begin[segment.first];
+    }
+
+    Frequency it_begin = find_max_frequency(f_begin);
+    Point begin;
+    if (it_begin->second == 1 && it_end->second > 1)
+        begin = f_begin.rbegin()->first;
+    else
+        begin = it_begin->first;
+
+    return std::pair<Point, Point>(begin, end);
 }
 } // namespace
 
 OverlapPointSet find_max_overlap_point(const LineSegmentSet &set)
 {
-    Frequencies f = get_overlap_frequency(set);
-    uint32_t max_frequency = get_max_frequency(f);
+    const std::pair<Point, Point> boundaries = find_search_boundaries(set);
+    const Frequencies f = get_overlap_frequency(set, boundaries.first, boundaries.second);
+    const uint32_t max_frequency = find_max_frequency(f)->second;
+
     OverlapPointSet result;
+    if (max_frequency == 1) // if there is no overlap
+        return result;      // return an empty set
 
     for (auto x : f)
     {
